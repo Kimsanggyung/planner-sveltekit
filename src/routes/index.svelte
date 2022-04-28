aa1e478ab502ee3e3114599b88fb672eabd72594
Kim sanggyung <ksang0906@gmail.com>
2022-04-28 16:27:31 +0900

    일정 컴포넌트 추가

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,12 @@
 <script>
 	import '../app.css';
-	import Calender from './calender.svelte';
 	import Clock from './clock.svelte';
+	import Calender from './calender.svelte';
+	import Todo from './todo.svelte';
 </script>
 <div>
 	<h1>현재시각</h1>
-	<Calender/>
 	<Clock/>
+	<Calender/>
 </div>
+<Todo/>
