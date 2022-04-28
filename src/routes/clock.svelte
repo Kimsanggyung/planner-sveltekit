eec84ce110d9b7c3af54bddcddf18e652e6392b3
Kim sanggyung <ksang0906@gmail.com>
2022-04-28 14:15:15 +0900

    오늘 날짜를 받아서 한국식으로 화면구현

---
+++ src/routes/clock.svelte
@@ -0,0 +1,11 @@
+<script>
+  const time = new Date();
+  const getTime = {
+    hour: time.getHours(),
+    minutes: time.getMinutes(),
+    seconds: time.getSeconds(),
+  }
+
+</script>
+
+<h1>{getTime.hour}시 {getTime.minutes}분 {getTime.seconds}초</h1>
