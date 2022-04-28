91e40bdf7d5b73d8d9efc5e0fed3dd2564475d81
Kim sanggyung <ksang0906@gmail.com>
2022-04-28 16:27:11 +0900

    라이브러리 install

--- src/routes/calender.svelte
+++ src/routes/calender.svelte
@@ -1,10 +1,13 @@
 <script>
-  let date = new Date();
-  let getDate ={
+	import { Datepicker } from 'svelte-calendar';
+  const date = new Date();
+  const getDate ={
     year: date.getFullYear(),
     month: date.getMonth(),
     day: date.getDay()
   }
 </script>
+  <Datepicker/>
+  <h1>{getDate.year}년 {getDate.month}월 {getDate.day}일</h1>
 
-<h1>{getDate.year}년 {getDate.month}월 {getDate.day}일</h1>
+  
