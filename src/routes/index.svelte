917f7a0764369c80bae299e0c36dd949f8e8e05b
Kim sanggyung <ksang0906@gmail.com>
2022-04-29 15:04:52 +0900

    조건문 추가 일정 상황에서만 그화면을 보이게 하기 위해서

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,14 +1,18 @@
 <script>
 	import '../app.css';
+	import { stateData } from '../store/store';
 	import Clock from '../components/clock.svelte';
-	//import Calender from '../components/today.svelte';
 	//import Todo from '../components/todo.svelte';
-	//import Month from '../components/month.svelte';
+	import Month from '../components/month.svelte';
 	import Select from '../components/select.svelte';
-import Today from '../components/today.svelte';
+	import Today from '../components/today.svelte';
+
 </script>
 <div>
 	<Today/>
 	<Clock/>
 </div>
+{#if !stateData.monthState}
+	<Month/>
+{/if}
  <Select/>
