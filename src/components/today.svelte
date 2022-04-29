3357ad93d76f03620749eb9a2974822e198959de
Kim sanggyung <ksang0906@gmail.com>
2022-04-29 14:58:05 +0900

    현재 날짜를 하면에 보이도록하는 기능

--- src/components/today.svelte
+++ src/components/today.svelte
@@ -1,15 +1,13 @@
 <script>
 	//import { Datepicker } from 'svelte-calendar';
   const date = new Date();
-  const year = date.getFullYear();
-  const month= date.getMonth() + 1;
-  const day = date.getDate();
-  const korean = date.toLocaleDateString();
+  const getDate = {
+    year:  date.getFullYear(),
+    month: date.getMonth() + 1,
+    day: date.getDate(),
+    korean: date.toLocaleDateString()
+  }
   
-  console.log(date)
-  console.log(month)
-  console.log(day)
-  console.log(korean)
 </script>
-  <h1>오늘날짜: {year}년 {month}월 {day}일</h1>
+  <button>오늘날짜: {getDate.year}년 {getDate.month}월 {getDate.day}일</button>
   
