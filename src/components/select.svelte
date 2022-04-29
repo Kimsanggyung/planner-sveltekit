6392248a1b8d159d7723760b1a2c36c106792bc0
Kim sanggyung <ksang0906@gmail.com>
2022-04-29 15:02:47 +0900

    다른화면을 보이게 하기 위한 함수 날짜를 받아올 변수 추가

--- src/components/select.svelte
+++ src/components/select.svelte
@@ -1,10 +1,33 @@
 <script>
-  
+
+  import { getDates, stateData } from '../store/store'
+
+  let getYear;
+  let getMonth;
+  let getDay;
+
+  getDates.subscribe(value => {
+   getYear = value.year
+   getMonth = value.month
+   getDay = value.day
+  })
+
+
+  const viweMonth = () => stateData.monthState = !stateData.monthState
+
+  const viweDaily = () => {stateData.monthState} 
+
+
 </script>
 
+<div>
+  <button>-</button>
+  <h1>{getYear}년 {getMonth}월 {getDay}일</h1>
+  <button on:click={toNextDay}>+</button>
+</div>
 
 <div>
-  <button>월간 </button>
+  <button on:click={viweMonth}>월간</button>
   <button>주간</button>
   <button>일간</button>
 </div>
