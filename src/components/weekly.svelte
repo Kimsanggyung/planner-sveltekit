88a5f2b98c19662868aa8b41d31c460845635fc7
Kim sanggyung <ksang0906@gmail.com>
2022-05-12 11:27:38 +0900

    이번주 날짜를 가져오는 변수와 형태를 지정해주는 함수 추가

--- src/components/weekly.svelte
+++ src/components/weekly.svelte
@@ -1,8 +1,31 @@
 <script>
-  import{getDates} from '../store/store'
+// @ts-nocheck
+
+  const getDate = new Date()
+  let year = getDate.getFullYear();
+  let month = getDate.getMonth();
+  let date = getDate.getDate();
+  let week = getDate.getDay();
+
+  let weekStartDate = new Date(year, month, date - week);
+  let weekEndDate = new Date(year, month, date + (6 - week));
+
+  let weekMonth;
+  let weekDay;
+  const formatDate = (date) => {
+    weekMonth = date.getMonth()+1;
+    weekDay = date.getDate();
+    return (weekMonth + "." + weekDay);
+  }
+
+  let getWeekStart = formatDate(weekStartDate)
+  let getWeekEnd = formatDate(weekEndDate)
+
 </script>
 
 <div>
+  <button>zzzz</button>
+  <div>{getWeekStart}-{getWeekEnd}</div>
   <span>일</span>
   <span>월</span>
   <span>화</span>
