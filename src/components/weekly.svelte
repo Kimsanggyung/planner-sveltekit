6059a9afe8ac6033fb8548f67bff8fe3b3bfc9a7
Kim sanggyung <ksang0906@gmail.com>
2022-05-12 15:50:40 +0900

    이번주 저번주를 확인할수 있게 해주는 버튼 생성 및 기능구현

--- src/components/weekly.svelte
+++ src/components/weekly.svelte
@@ -3,29 +3,67 @@
 
   const getDate = new Date()
   let year = getDate.getFullYear();
-  let month = getDate.getMonth();
+  let month = getDate.getMonth()+1;
   let date = getDate.getDate();
   let week = getDate.getDay();
 
   let weekStartDate = new Date(year, month, date - week);
   let weekEndDate = new Date(year, month, date + (6 - week));
 
-  let weekMonth;
+  let weekMonth = getDate.getMonth();
   let weekDay;
-  const formatDate = (date) => {
-    weekMonth = date.getMonth()+1;
-    weekDay = date.getDate();
-    return (weekMonth + "." + weekDay);
-  }
 
+  const formatDate = (date) => {
+    weekDay = date.getDate();
+    return (weekDay);
+  }
+  
   let getWeekStart = formatDate(weekStartDate)
   let getWeekEnd = formatDate(weekEndDate)
 
+  const nextWeek = () => {
+    const checkMonth = [1,3,5,7,8,10,12]
+    const result = checkMonth.find(check => check == month)
+    if(month >=12){
+      year+1
+    }
+    if(result !== undefined && getWeekStart >= 24){
+      month +=1;
+      return getWeekStart = (getWeekStart+7)-31
+    }
+    if(result !== undefined && getWeekEnd >= 24){
+      weekMonth +=1;
+      return getWeekEnd = (getWeekEnd+7)-31
+    }
+    getWeekStart+= 7;
+    getWeekEnd += 7;
+  }
+
+  const prevWeek = () => {
+    if(month <=1 ){
+      year -=1;
+      return month = 12;
+    }
+    if(getWeekStart <= 1){
+      month -= 1;
+    }
+    if(getWeekEnd <=1){
+      weekMonth -= 1;
+    }
+    getWeekStart -= 7;
+    getWeekEnd -= 7;
+  }
+
+  
 </script>
 
 <div>
-  <button>zzzz</button>
-  <div>{getWeekStart}-{getWeekEnd}</div>
+  <div>
+    <div>{year}년</div>
+    <button on:click={prevWeek}>저번주</button>
+    <div>{month}.{getWeekStart}-{weekMonth+1}.{getWeekEnd}</div>
+    <button on:click={nextWeek}>다음주</button>
+  </div>
   <span>일</span>
   <span>월</span>
   <span>화</span>
