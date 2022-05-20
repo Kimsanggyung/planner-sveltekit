dc32b5d8587bf130f4be02a85acaba68be139f84
Kim sanggyung <ksang0906@gmail.com>
2022-05-20 12:34:18 +0900

    시간 선택 추가, 입력창에 선택한 일정 정보를 넣어주는 코드 추가

--- src/components/editTodo.svelte
+++ src/components/editTodo.svelte
@@ -11,12 +11,15 @@
   let date = getDate.getDate();
 
   let edidtDatas ={
-    setDate: year + '.' + month + '.' + date,
+    setDate: '',
     setTodo: '',
     setDetails: ''
   }
 
-  
+  const time = [
+    {num:1},{num:2},{num:3},{num:4},{num:5},{num:6},{num:7},{num:8},{num:9},{num:10},
+    {num:11},{num:12},{num:13},{num:14},{num:15},{num:16},{num:17},{num:18},{num:19},{num:20},{num:21},{num:22},{num:23},{num:24}
+  ]
 
   year + '.' + month + '.' + date
 
@@ -36,7 +39,18 @@
     })
   }
 
-  
+  const getDataValue = ()=> {
+    let store = db.transaction('datas', 'readonly').objectStore('datas')
+    let getReq = store.get(1);
+    getReq.addEventListener('success', function(event){
+      let value = event.target.result
+      console.log(value)
+      edidtDatas.setDate = value.setTodoList.setDate;
+      edidtDatas.setTodo = value.setTodoList.setTodo;
+      edidtDatas.setDetails = value.setTodoList.setDetails
+    })
+  }
+  getDataValue();
 
   const cancel = () =>{
     stateData.update(state => {
@@ -52,6 +66,11 @@
 <input bind:value={edidtDatas.setDate}>
 <input bind:value={edidtDatas.setTodo}>
 <input bind:value={edidtDatas.setDetails}>
+<select id="time" >
+    {#each time as {num}, i}
+    <option value={num}> {num}시 </option>
+    {/each}
+</select>
 
-<button on:click={cancel}>취소</button>
 <button on:click={editTodoDatas}>등록</button>
+<button on:click={cancel}>취소</button>
