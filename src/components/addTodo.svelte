f2217885f175ac59f87015f6a1cacc49f872f8a4
Kim sanggyung <ksang0906@gmail.com>
2022-05-25 17:59:36 +0900

    일정을 추가하는 컴포넌트 추가

--- src/components/addTodo.svelte
+++ src/components/addTodo.svelte
@@ -1,27 +1,29 @@
 <script>
 // @ts-nocheck
-  import localforage from 'localforage'
+  import { todoDate } from '../store/store'
   import { db } from '../store/indexed'
+
+  let text;
+  let value;
   let todo = '';
   let details = '';
   let getDate = new Date();
   let year = getDate.getFullYear();
   let month = getDate.getMonth()+1;
   let date = getDate.getDate();
-
   let setDate = year + '.' + month + '.' + date
 
   $: setTodoList = {
 	  setTodo: todo,
 	  setDetails: details,
-    setDate: setDate
+    setDate: setDate,
+    setTime: value
   }
 
   const time = [
     {num:1},{num:2},{num:3},{num:4},{num:5},{num:6},{num:7},{num:8},{num:9},{num:10},
     {num:11},{num:12},{num:13},{num:14},{num:15},{num:16},{num:17},{num:18},{num:19},{num:20},{num:21},{num:22},{num:23},{num:24}
   ]
-
   const setItem =() =>{
     let store = db.transaction('datas', 'readwrite').objectStore('datas')
     let addReq = store.add({setTodoList})
@@ -30,15 +32,15 @@
     })
   }
 
-  /*const timeValue = (target) => {
-    console.log(target.value);
-    console.log(target.options[target.selectedIndex].text);
+  const timeValue = () => {
+    let target = document.getElementById("time")
+    value = target.options[target.selectedIndex].value;
+    text =  target.options[target.selectedIndex].text;
+    console.log(value)
+    console.log(text)
   }
-  */
-
+ 
 </script>
-
-<button on:click={setItem}>등록</button>
 
 <div>
   <span>제목: </span>
@@ -52,13 +54,14 @@
 
 <div>
   <label for="start" >예정일:</label>
-  <input type="text" name="start" bind:value={setDate} class="border border-gray-500 w-24">
-  <select id="time">
+  <input type="text" name="start" bind:value={$todoDate} class="border border-gray-500 w-24">
+  <select id="time" on:change={timeValue}>
+    <option value="not">시간선택</option>
     {#each time as {num}, i}
-    <option value="" >{num}시 </option>
+    <option value={num}> {num}시 </option>
     {/each}
   </select>
 </div>
 
-
+<button on:click={setItem}>등록</button>
 
