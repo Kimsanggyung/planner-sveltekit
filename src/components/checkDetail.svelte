484b727ccf860afab365f3413cafa4ab8790046c
Kim sanggyung <ksang0906@gmail.com>
2022-06-30 11:15:17 +0900

    수정할수 있는 기능 구현

--- src/components/checkDetail.svelte
+++ src/components/checkDetail.svelte
@@ -1,27 +1,43 @@
 <script>
   // @ts-nocheck
   
-    import { todoDatas } from '../store/store';
+    import { todoDatas,stateData } from '../store/store';
     import { getItem } from '../store/indexed';
     
     let targetID;
-    let day;
+    let day; 
     let todo;
+    let time;
     let details;
     let getItemPromise = null;
 
     todoDatas.subscribe(value => {
+      targetID = value.editTargetID
     getItemPromise = getItem(value.editTargetID).then(data => {
       console.log(data)
         day = data.setTodoList.setDate;
         todo = data.setTodoList.setTodo;
         details = data.setTodoList.setDetails;
+        time = data.setTodoList.setTime;
         return data;
       });
   })
 
-  
-  
+  const editMode = (id) =>{
+    stateData.update(state => {
+      state.edidtTodoState = true;
+      return state;
+    })
+    stateData.update(state =>{
+      state.checkDetailState = false;
+      return state
+    })
+    todoDatas.update(value => {
+      value.editTargetID = id
+      return value
+    })
+  }
+
   </script>
 
 {#await getItemPromise}
@@ -39,10 +55,15 @@
         <span>{details}</span>
       </div>
   
-      <div class="mb-10 text-2xl">
+      <div class="text-2xl">
         <span>예정일:</span>
         <span>{day}</span>
       </div>
+      <div class="mb-10 text-2xl">
+        <span>예정시간:</span>
+        <span>{time}시</span>
+      </div>
+      <button on:click={() => editMode(targetID)} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">수정</button>
     </div>
   </div>
 {/await}
