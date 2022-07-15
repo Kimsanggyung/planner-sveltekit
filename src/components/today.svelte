3ebff875e0cfe97d83632bfc25303d27f8b84b4d
Kim sanggyung <ksang0906@gmail.com>
2022-07-15 13:17:19 +0900

    날짜 포멧변경

--- src/components/today.svelte
+++ src/components/today.svelte
@@ -1,16 +1,20 @@
 <script>
-	//import { Datepicker } from 'svelte-calendar';
-  const date = new Date();
-  const getDate = {
-    year:  date.getFullYear(),
-    month: date.getMonth() + 1,
-    day: date.getDate(),
-    korean: date.toLocaleDateString()
+// @ts-nocheck
+  const getDate = new Date();
+  let year =  getDate.getFullYear()
+  let month = getDate.getMonth() + 1
+  let day = getDate.getDate()
+  
+  const dateFormat = (date) =>{
+    month = month >= 10 ? month : '0' + month;
+    day = day >= 10 ? day : '0'+day;
+    return year + '년 ' + month + '월 ' + day + '일'
   }
+  dateFormat()
   
 </script>
 
-<span class="flex items-center font-jua ml-4 mt-2">
-  <button class="text-3xl">오늘날짜: {getDate.year}년 {getDate.month}월 {getDate.day}일</button>
-</span>
+<div class="flex items-center font-jua ml-4 mt-2">
+  <button class="text-3xl">오늘날짜:{year}년 {month}월 {day}일</button>
+</div>
   
